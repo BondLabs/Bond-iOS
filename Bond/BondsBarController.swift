36d7d244a549058aaab4c782e26a5e1e75fd8f12
dominion500 <kevin.zhang500@gmail.com>
2014-12-30 04:23:12 -0800

    Bonds and Profile View
    Did basic set up for bonds and profile view (after logging in).  Also
    did some more cleaning up and implemented class hierarchies

---
+++ Bond/BondsBarController.swift
@@ -0,0 +1,30 @@
+//
+//  BondsBarController.swift
+//  Bond
+//
+//  Created by Kevin Zhang on 12/30/14.
+//  Copyright (c) 2014 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class BondsBarController: UITabBarController {
+    
+    var views:[UIViewController]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        views = self.viewControllers as [UIViewController]
+    }
+
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
+}
---
+++ Bond/BondsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BondsViewController.swift
+//  Bond
+//
+//  Created by Kevin Zhang on 12/30/14.
+//  Copyright (c) 2014 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class BondsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = self.UIColorFromRGB(0x2D2D2D)
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
+}
