81945f03e34d3f32455cf136050e3aaa5bd50e21
Kevin Zhang <kevin@bond.sh>
2015-02-08 21:14:28 -0500

    fixed code errors
    no code errors left - should be working…?

--- Bond/UserAccountController.swift
+++ Bond/UserAccountController.swift
@@ -25,7 +25,7 @@ class UserAccountController: NSObject, NSURLConnectionDelegate, NSURLConnectionD
     var id: Int = 1
     let authKey = "37D74DBC-C160-455D-B4AE-A6396FEE7954"
     var isUserLoggedIn = false
-    var otherUserImages:[String: UIImage]! = []
+	var otherUserImages:[String: UIImage]! = [:]
 
     class var sharedInstance: UserAccountController {
         struct Static {
